884b53583b5cf5ef0be5394af0ffabf13ae96580
Stupel <hellomariandev@gmail.com>
2024-03-17 16:18:37 +0100

    application init

---
+++ app/layout.jsx
@@ -0,0 +1,28 @@
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+import "./globals.css";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata = {
+  title: "Advanced Counter",
+  description: "State management in React Js",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="">
+      <h1 className="text-center text-3xl font-extralight my-12">Advanced Counter</h1>
+    </main>
+  );
+}
