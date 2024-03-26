590d0455216f9ec0ac8d4ed32159d118becf1aa9
Stupel <hellomariandev@gmail.com>
2024-03-26 11:33:37 +0100

    imported from useState

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,46 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { formatNumber } from "@/lib/format-number";
+import { useState } from "react";
 
 export default function Home() {
+  const [count, setCount] = useState(2);
+  const [value, setValue] = useState({
+    increment: 1,
+    decrement: 1,
+    multiplication: 4,
+    division: 3,
+    exponentiation: 2,
+  });
+
+  const handleValueChange = (e, operation) => {
+    setValue((prevValue) => ({
+      ...prevValue,
+      [operation]: parseInt(e.target.value),
+    }));
+  };
+
+  function increment() {
+    setCount((prevCount) => (prevCount += value.increment));
+  }
+  const decrement = () => {
+    setCount((prevCount) => prevCount - value.decrement);
+  };
+  function multiplication() {
+    setCount((prevCount) => (prevCount *= value.multiplication));
+  }
+  function division() {
+    setCount((prevCount) => (prevCount /= value.division));
+  }
+  function exponentiation() {
+    setCount((prevCount) => (prevCount **= value.exponentiation));
+  }
+
+  function reset() {
+    setCount(0);
+  }
   return (
     <main className="">
       <h2 className="text-center text-3xl font-extralight mt-12 mb-4">
@@ -9,50 +48,72 @@ export default function Home() {
       </h2>
 
       <div className="w-full flex flex-col md:flex-row justify-evenly items-center">
-        <div className="md:order-1 flex flex-col gap-y-2 items-center m-4 md:m-8">
+        <div className="md:order-1 flex flex-col gap-y-2 items-center m-8">
           <div className="flex flex-row gap-x-2">
-            <Input placeholder="+1" className="w-16 text-center font-medium" />
-            <Button className="w-36" variant="outline">
+            <Input
+              placeholder="+1"
+              className="w-16 text-center font-medium"
+              onChange={(e) => handleValueChange(e, "increment")}
+            />
+            <Button className="w-36" variant="outline" onClick={increment}>
               Addition
             </Button>
           </div>
 
           <div className="flex flex-row gap-x-2">
-            <Input placeholder="-1" className="w-16 text-center font-medium" />
-            <Button className="w-36" variant="outline">
+            <Input
+              placeholder="-1"
+              className="w-16 text-center font-medium"
+              onChange={(e) => handleValueChange(e, "decrement")}
+            />
+            <Button className="w-36" variant="outline" onClick={decrement}>
               Subtraction
             </Button>
           </div>
 
           <div className="flex flex-row gap-x-2">
-            <Button className="w-44" variant="destructive">
+            <Button className="w-44" variant="destructive" onClick={reset}>
               Reset
             </Button>
           </div>
         </div>
 
         <div className="order-first md:order-2">
-          <h1 className="text-center text-7xl font-extrabold">0</h1>
+          <h1 className="text-center text-7xl font-extrabold">
+            {formatNumber(count)}
+          </h1>
         </div>
 
-        <div className="md:order-3 flex flex-col gap-y-2 items-center m-4 md:m-8">
+        <div className="md:order-3 flex flex-col gap-y-2 items-center m-8">
           <div className="flex flex-row gap-x-2">
-            <Input placeholder="*4" className="w-16 text-center font-medium" />
-            <Button className="w-36" variant="outline">
+            <Input
+              placeholder="*4"
+              className="w-16 text-center font-medium"
+              onChange={(e) => handleValueChange(e, "multiplication")}
+            />
+            <Button className="w-36" variant="outline" onClick={multiplication}>
               Multiplication
             </Button>
           </div>
 
           <div className="flex flex-row gap-x-2">
-            <Input placeholder="/3" className="w-16 text-center font-medium" />
-            <Button className="w-36" variant="outline">
+            <Input
+              placeholder="/3"
+              className="w-16 text-center font-medium"
+              onChange={(e) => handleValueChange(e, "division")}
+            />
+            <Button className="w-36" variant="outline" onClick={division}>
               Division
             </Button>
           </div>
 
           <div className="flex flex-row gap-x-2">
-            <Input placeholder="**2" className="w-16 text-center font-medium" />
-            <Button className="w-36" variant="outline">
+            <Input
+              placeholder="**2"
+              className="w-16 text-center font-medium"
+              onChange={(e) => handleValueChange(e, "exponentiation")}
+            />
+            <Button className="w-36" variant="outline" onClick={exponentiation}>
               Exponentiation
             </Button>
           </div>
